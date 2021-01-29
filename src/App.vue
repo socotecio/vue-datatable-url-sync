c22eb53c9a8ef11df5ef946655c90927fbbacec3
Adrien Montagu <adrienmontagu@gmail.com>
2021-01-29 10:45:14 +0100

    lint recommended

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  >
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
